4d059f9b4e2657c86f1aa44d54cd0f6e603bae96
disco07 <koneenok@outlook.fr>
2022-09-20 12:00:45 +0200

    test

--- main_test.go
+++ main_test.go
@@ -24,3 +24,33 @@ func TestScrap(t *testing.T) {
 		})
 	}
 }
+
+func TestExcelizeData(t *testing.T) {
+	d := []*Data{
+		{
+			Name:    "Drissa KONE",
+			Title:   "Software developer",
+			Company: "Freelance",
+		},
+	}
+	tests := []struct {
+		description string
+		data        []*Data
+		expected    error
+	}{
+		{
+			description: "create file xls with data",
+			data:        d,
+			expected:    nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			err := excelizeData(tt.data)
+			if tt.expected == nil && err != nil {
+				t.Errorf("got %v want %v", err.Error(), tt.expected)
+			}
+		})
+	}
+}
